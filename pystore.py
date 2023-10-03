636bc69ed69fc758be978cc7ca669be1ad851565
zayoon013 <zain.abudaqqa@icloud.com>
2023-10-03 17:19:01 +0300

    Create pystore.py

---
+++ pystore.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+os.system("alias python=python3")
+os.system("echo 'alias python=python3' >> ~/.bash_profile")
+os.system("echo 'alias python=python3' >> ~/.zshrc")
+
+
+print("Welcome to PyStore")
+
+print("List of programs Available to download and install:")
+print("")
