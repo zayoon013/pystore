16f7358583c6fefd9d9288afe279bf61c1c2be65
zayoon013 <zain.abudaqqa@icloud.com>
2023-10-03 17:27:28 +0300

    Update pystore.py

--- pystore.py
+++ pystore.py
@@ -9,4 +9,10 @@ os.system("echo 'alias python=python3' >> ~/.zshrc")
 print("Welcome to PyStore")
 
 print("List of programs Available to download and install:")
-print("")
+print("pycodeapp")
+a = int(input())
+if t == 1:
+	os.system("git clone https://github.com/zayoon013/pycodeapp.git && cd pycodeapp && ")
+	
+
+	
